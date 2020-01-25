fc01ed61f649da11ba6d71723ac82f478b962a9b
aashishghogre <aghogre@equalexperts.com>
2020-01-25 07:17:57 +0530

    Add Sleep to test to wait for apis to start

--- api/src/app/todo_test.go
+++ api/src/app/todo_test.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"os"
 	"testing"
-
+	"time"
 	"github.com/DATA-DOG/godog"
 	"github.com/DATA-DOG/godog/colors"
 )
@@ -18,6 +18,7 @@ func FeatureContext(s *godog.Suite) {
 func TestMain(m *testing.M) {
 	flag.Parse()
 	go func() { startServer(8083, "todo_test.db") }()
+	time.Sleep(1 * time.Second)
 	status := godog.RunWithOptions("godogs", FeatureContext, godog.Options{
 		Output: colors.Colored(os.Stdout),
 		Format: "progress", // can define default values
